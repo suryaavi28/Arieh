901267d114f1afbdc27e09e22a574d9707aa990d
Avinash <avisurya1@adani.com>
2024-11-12 10:58:28 +0530

    new pom changes

--- src/test/java/com/arieh/tests/Arieh_UAT_AddDispatchPageTest.java
+++ src/test/java/com/arieh/tests/Arieh_UAT_AddDispatchPageTest.java
@@ -1,6 +1,6 @@
 package com.arieh.tests;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.By;	
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
